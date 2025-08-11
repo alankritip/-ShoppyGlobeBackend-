007fa6c5821c46ca3013aef866dcf23f316cd738
alankritip <alankritipal2004@gmail.com>
2025-08-11 16:48:06 +0530

    Error Handler to format errors

---
+++ middleware/errorHandler.js
@@ -0,0 +1,5 @@
+// Central error handler to format errors
+module.exports = (err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).json({ message: err.message || "Server Error" });
+};
