5eae2b231df5ad64106def6bfaa37e36d153d229
alankritip <alankritipal2004@gmail.com>
2025-08-11 16:43:55 +0530

    GET all products with and without id

---
+++ controllers/productController.js
@@ -0,0 +1,22 @@
+const Product = require('../models/Product');
+
+// GET /products
+exports.getAllProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// GET /products/:id
+exports.getProductById = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: 'Product not found' });
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+};
